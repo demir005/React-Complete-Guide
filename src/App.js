0a79055413cf21fd2d617b2cefe182bdb31e48db
demir005 <demirmah93@gmail.com>
2022-04-12 16:50:17 +0200

    Create first Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpensItem from "./components/ExpensItem";
+
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpensItem></ExpensItem>
     </div>
   );
 }
---
+++ src/components/ExpensItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css';
+
+function ExpensItem() {
+   const expenseDate = new Date(2022,3,12);
+   const expenseTitle = 'Car Insurance';
+   const expenseAmount = 530.25;
+
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensItem;

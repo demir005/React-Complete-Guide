693bfdc3a31945985475b9531836f0ee0855a86e
demir005 <demirmah93@gmail.com>
2022-04-20 17:06:12 +0200

    ChartBar added to show graphicaly expenses by mounth and year

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,29 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
 import './ExpensesList.css';
 
-const ExpensesList = props => {
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
 
-    if(props.item.lenght === 0){
-        return <h2 className="expenses-list__fallback">Found no expenses. </h2>;
-    }
-
-    return (
-        <ul className="expenses-list">
-        {
-            props.item.map((expense) => (
-               <ExpenseItem
-                 key={expense.id}
-                 title={expense.title}
-                 amount={expense.amount}
-                 date={expense.date}
-               />
-           ))}
-       </ul>
-    );
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 };
-
-
 
 export default ExpensesList;
